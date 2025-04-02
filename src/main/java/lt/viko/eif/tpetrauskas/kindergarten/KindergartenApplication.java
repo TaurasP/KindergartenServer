886c1a8787628b8633be848ec940fedc20d65395
Tauras Petrauskas <petrauskas.post@gmail.com>
2025-04-02 13:14:49 +0300

    Initial commit

---
+++ src/main/java/lt/viko/eif/tpetrauskas/kindergarten/KindergartenApplication.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.tpetrauskas.kindergarten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindergartenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KindergartenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lt/viko/eif/tpetrauskas/kindergarten/KindergartenApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.tpetrauskas.kindergarten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindergartenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
